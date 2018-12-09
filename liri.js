98052d51992bdfe2f468eec762f3877893edef45
vaguilar25 <vivian.aguilar@gmail.com>
2018-12-08 20:24:18 -0600

    read files

--- liri.js
+++ liri.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 var Spotify = require("node-spotify-api");
 var request = require("request");
 var moment = require("moment");
+var fs = require("fs");
 //var spotify = new Spotify(keys.spotify);
 //var spotify = new Spotify({
 //  id: 'cc12f274c15340fe8072c90f855dab40',
 //secret: 'f44c5fc75faf429f9c1a4e59a3f394ee'
 //});
+
+
 var spotify = new Spotify({
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
@@ -18,44 +21,57 @@ var spotify = new Spotify({
 var userSelection = process.argv[2];
 var userInput = process.argv;
 
-//If the command is to search a song in spotify
-var userInputConcat = ""
-concatenateUserSelection();
+//concatenate user inpit
+var userInputConcat = "";
+var textSong="";
+concatenateUserInput(userInput);
 
-switch (userSelection) {
-    case "spotify-this-song":
+//Run the program
+run(userSelection);
 
-        if (userInputConcat.length != 0) {
-            search_track(userInputConcat.trim());
-        } else {
-            search_track("The Sign Ace of Base");
-        }
-        break;
-    case "concert-this":
+//Run Function to handle all the inputs of the user
+function run(userSelection) {
+   
+    switch (userSelection) {
+        case "spotify-this-song":
+
+            if (userInputConcat.length != 0) {
+                search_track(userInputConcat.trim());
+            } else if (textSong.length != 0) {
+                search_track(textSong.trim());
+            } else {
+                search_track("The Sign Ace of Base");
+            }
+            break;
+        case "concert-this":
 
 
-        getConcertInfo(userInputConcat);
-        break;
+            getConcertInfo(userInputConcat);
+            break;
 
-    case "movie-this":
-        if (userInputConcat.length != 0) {
+        case "movie-this":
             var displayNobodyInfo = false;
-            getMovieInfo(userInputConcat.trim(),displayNobodyInfo );
+            if (userInputConcat.length != 0) {
+                getMovieInfo(userInputConcat.trim(), displayNobodyInfo);
 
-        } else {
-            var displayNobodyInfo = true;
-            getMovieInfo("Mr. Nobody", displayNobodyInfo);
-        }
-        break;
-    default:
-        break;
+            } else {
+                displayNobodyInfo = true;
+                getMovieInfo("Mr. Nobody", displayNobodyInfo);
+            }
+            break;
+        case "do-what-it-says":
+            readFile();
+            break;
+        default:
+            break;
 
+    }
 }
-
 //function to  concatenate User Info
 
-function concatenateUserSelection() {
+function concatenateUserInput(userInput) {
     //concatenate what user is searching
+    //  console.log(userInput);
     for (i = 3; i < userInput.length; i++) {
         userInputConcat = userInputConcat + " " + userInput[i];
     }
@@ -130,7 +146,7 @@ function search_track(song) {
 function getMovieInfo(movieName) {
     var queryURL = "https://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-   // console.log(queryURL);
+    // console.log(queryURL);
     request(queryURL, function (error, response, body) {
 
         if (!error && response.statusCode === 200) {
@@ -155,7 +171,7 @@ function getMovieInfo(movieName) {
             console.log("Actors in the movie: " + movie.Actors);
 
             if (displayNobodyInfo) {
-                
+
                 console.log("If you haven't watched \"Mr. Nobody,\" then you should: http://www.imdb.com/title/tt0485947/");
 
                 console.log("It's on Netflix!");
@@ -165,4 +181,25 @@ function getMovieInfo(movieName) {
             console.log(error)
         }
     })
+}
+
+
+//Read File
+function readFile() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        //   console.log("Data: " + data);
+        var command = JSON.stringify(data);
+        var splitCommand = data.split(",");
+        // console.log(splitCommand[1]);
+        textSong = splitCommand[1];
+        run(splitCommand[0], textSong);
+        //console.log(JSON.stringify(data));
+
+
+
+    })
+
 }
